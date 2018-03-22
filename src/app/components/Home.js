31c886b3e81bb153a82a1ddfb47129cd64f26001
imar26 <aadeshranderia26@gmail.com>
2018-03-22 01:33:03 -0400

    display data, pass data using props

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,16 +1,30 @@
 import React from "react";
+import PropTypes from 'prop-types'
 
 export class Home extends React.Component {
     render() {
-        let content = "";
-        if(true) {
-            content = <p>Hello</p>
-        }
+        var text = "Something!";
         return (
             <div>
                 <p>In a new component!</p>
-                {content}
+                <p>{text}</p>
+                <p>Your name is {this.props.name}</p>
+                <p>Your age is {this.props.age}</p>
+                <p>User Object => Name: {this.props.user.name}</p>
+                <p>Hobbies</p>
+                <ul>
+                    {this.props.user.hobbies.map((hobby, i) => <li key={i}>{hobby}</li>)}
+                </ul>
+                <hr/>
+                {this.props.children}
             </div>
         );
     }
 }
+
+Home.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number,
+    user: PropTypes.object,
+    children: PropTypes.element.isRequired
+};
